99a473c13f4e02b72b793c170b4e0e8a8065712f
twinkalp10 <twinkal.work@outlook.com>
2026-01-27 21:16:49 +0530

    fix docker compose

--- local/init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create the inngest database if it doesn't exist
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE inngest' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'inngest')\gexec
-EOSQL
